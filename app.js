19c7c303ba2d8b39c0fa41bad98044b51493df82
simplesoftwaredev <89336369+simplesoftwaredev@users.noreply.github.com>
2021-10-18 09:29:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLink = document.getElementsByClassName('navbar-link')[0]
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video");
 
-toggleButton.addEventListener('click', () => {
-    navbarLink.classList.toggle('active')
+btn.addEventListener("click", function() {
+    if (!btn.classList.contains("slide")) {
+        btn.classList.add("slide")
+        video.pause()
+    }
+    else {
+        btn.classList.remove("slide")
+        video.play()
+    }
+})
+
+const toggleBar = document.getElementsByClassName("toggle-bar")[0];
+const navLink = document.getElementsByClassName("nav-link")[0];
+
+
+toggleBar.addEventListener("click", () => {
+    navLink.classList.toggle("active")
 })
