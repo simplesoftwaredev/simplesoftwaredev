5dfb39bdab13191fa188a8bd2b8d121ed5039242
simplesoftwaredev <89336369+simplesoftwaredev@users.noreply.github.com>
2021-12-12 09:07:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-const btn = document.querySelector(".switch-btn");
-const video = document.querySelector(".video");
+const target = document.getElementById("target");
+let array = ["Freelancer", "Designer", "Developer"];
+let wordIndex = 0;
+let letterIndex = 0;
 
-btn.addEventListener("click", function() {
-    if (!btn.classList.contains("slide")) {
-        btn.classList.add("slide")
-        video.pause()
-    }
-    else {
-        btn.classList.remove("slide")
-        video.play()
-    }
-})
+const createLetter = () => {
+    const letter = document.createElement("span")
+    target.appendChild(letter)
 
-const toggleBar = document.getElementsByClassName("toggle-bar")[0];
+
+    letter.textContent = array[wordIndex][letterIndex]
+    setTimeout(() => {
+        letter.remove()
+    }, 2000)
+};
+
+const loop = () => {
+    setTimeout(() => {
+        if (wordIndex >= array.length) {
+            wordIndex = 0;
+            letterIndex = 0;
+            loop()
+        } else if (letterIndex < array[wordIndex].length) {
+            createLetter()
+            letterIndex++
+            loop()
+        } else {
+            wordIndex++
+            letterIndex = 0;
+            setTimeout(() => {
+                loop()
+            }, 2000)
+        }
+    }, 50)
+};
+loop();
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navLink = document.getElementsByClassName("nav-link")[0];
 
-
-toggleBar.addEventListener("click", () => {
+toggleButton.addEventListener("click", () => {
     navLink.classList.toggle("active")
+    
 })
